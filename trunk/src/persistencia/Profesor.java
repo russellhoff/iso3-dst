58e4f49d1c8e4e4f6c801f5f10893a57b3d1ea17
captain06@gmail.com <captain06@gmail.com@0f77531e-132e-7bc0-bbd3-ecd54fb292ef>
2012-10-04 10:27:59 +0000

    Primera versión en la que funciona hibernate. Test con el profesor correcto. Se ha añadido un jar necesario (hibernate-jpa-2.0-api-1.0.1.Final.jar). Modificado el fichero hibernate.cfg.xml y añadidos los DTD de la configuración y el mapeo de clases (por si no se tiene conexión a internet).

--- trunk/src/persistencia/Profesor.java
+++ trunk/src/persistencia/Profesor.java
@@ -39,4 +39,70 @@ public class Profesor {
 		
 	}
 	
+	/**
+	 * <h1>Constructora acia</h1>
+	 * <p></p>
+	 */
+	public Profesor(){
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDespacho() {
+		return despacho;
+	}
+
+	public void setDespacho(String despacho) {
+		this.despacho = despacho;
+	}
+	
+	
+	
 }
