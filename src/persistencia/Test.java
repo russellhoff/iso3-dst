f9142305e4471595e1c860c105214d19c5eb6ffa
captain06@gmail.com <captain06@gmail.com@0f77531e-132e-7bc0-bbd3-ecd54fb292ef>
2012-10-04 10:27:59 +0000

    Primera versión en la que funciona hibernate. Test con el profesor correcto. Se ha añadido un jar necesario (hibernate-jpa-2.0-api-1.0.1.Final.jar). Modificado el fichero hibernate.cfg.xml y añadidos los DTD de la configuración y el mapeo de clases (por si no se tiene conexión a internet).

--- src/persistencia/Test.java
+++ src/persistencia/Test.java
@@ -15,8 +15,14 @@ public class Test {
 
 	SessionFactory sessionFactory;
 	
-	public Test(){
+	public Test() throws Exception{
+		
 		sessionFactory = new Configuration().configure().buildSessionFactory();
+		
+		if( sessionFactory.equals(null) ){
+			throw new Exception("Session factory is null");
+		}
+		
 	}
 	
 	
@@ -25,19 +31,25 @@ public class Test {
 		Session session = sessionFactory.openSession();
         Transaction tx = session.beginTransaction();
 		
-		Profesor unProfe = new Profesor(
-				78936044,
-				"1234",
-				"Juanito Panes",
-				"666228866",
-				"juan@panes.es",
-				"11ASK"
-				);
+		Profesor unProfe = new Profesor();
+		unProfe.setDni(78936044);
+		//unProfe.setId(1);
+		unProfe.setPassword("1234");
+		unProfe.setNombre("Pedro Zier");
+		unProfe.setTelefono("777886543");
+		unProfe.setEmail("pz23@juer.com");
+		unProfe.setDespacho("12AZ");
+		
 		
 		session.save(unProfe);
 		
 		tx.commit();
-		session.close();
+		
+		if( session != null ){
+			session.flush();
+			session.close();
+		}
+		
         System.out.println("Añadido profesor!");
 		
 	}
@@ -52,10 +64,14 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		
+		try{
+			Test t1 = new Test();
+			t1.prueba();
+			t1.close();
+		}catch(Exception e){
+			System.out.println("****Error: " + e.getMessage());
+		}
 		
-		Test t1 = new Test();
-		t1.prueba();
-		t1.close();
 		
 		
 	}
